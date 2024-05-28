2cfc347b730824f68b08fa4566af3700114906f4
ebagabe <gabriel.ebagabe@gmail.com>
2024-05-28 13:27:00 -0300

    Iniciando a Estilização

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
+import Input from '../src/components/Input'
+import Button from '../src/components/Button'
 import { Container, Content } from './styles';
 
 const App = () => {
   return (
     <Container>
       <Content>
-        Olá
+        <Input />
       </Content>
     </Container>
   )
--- src/components/Input/index.js
+++ src/components/Input/index.js
@@ -1,9 +1,11 @@
+import { InputContainer } from './styles'
+
 const Input = () => {
-    return (
-      <div className="app">
-  
-      </div>
-    )
-  }
-  
-  export default Input;
+  return (
+    <InputContainer>
+      <Input />
+    </InputContainer>
+  )
+}
+
+export default Input;
